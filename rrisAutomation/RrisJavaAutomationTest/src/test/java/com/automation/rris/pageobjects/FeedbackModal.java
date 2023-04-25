a5a5db2d24962df3a1f8f313b1a806946f2108e6
Irina Dotsenko <rokshata@yandex.ru>
2023-04-26 00:27:10 +0300

    Импорнт selenium во все классы + перечислены элементы для хеддера

--- rrisAutomation/RrisJavaAutomationTest/src/test/java/com/automation/rris/pageobjects/FeedbackModal.java
+++ rrisAutomation/RrisJavaAutomationTest/src/test/java/com/automation/rris/pageobjects/FeedbackModal.java
@@ -1,4 +1,7 @@
 package com.automation.rris.pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 public class FeedbackModal {
 }
--- rrisAutomation/RrisJavaAutomationTest/src/test/java/com/automation/rris/pageobjects/Header.java
+++ rrisAutomation/RrisJavaAutomationTest/src/test/java/com/automation/rris/pageobjects/Header.java
@@ -1,4 +1,30 @@
 package com.automation.rris.pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 public class Header {
+
+    private WebDriver driver;
+    //лого
+    private By Logo  = By.xpath("");
+    //выпадающий список городов
+    private By CitiesList  = By.xpath("");
+    //разделов ресторанов
+    private By Restaurants  = By.xpath("");
+    //раздел новостей
+    private By News  = By.xpath("");
+    //раздел акций
+    private By Promotions  = By.xpath("");
+    //раздел бонусов
+    private By Bonuses  = By.xpath("");
+    //бронирование столика
+    private By Reservation  = By.xpath("");
+    //корзина
+    private By Cart  = By.xpath("");
+    //логин
+    private By Login  = By.xpath("");
+    //профиль
+    private By Profile  = By.xpath("");
+
 }
